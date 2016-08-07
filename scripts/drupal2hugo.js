bde81080f35c93f65a48a1b502895f2d41258a01
Augustin Cavalier <waddlesplash@gmail.com>
2016-08-07 16:51:10 -0400

    drupal2hugo: Pseudo-parse and convert <code> tags properly, handle file formats.

--- scripts/drupal2hugo.js
+++ scripts/drupal2hugo.js
@@ -2,7 +2,7 @@
  * Drupal 6 CSV dump->Hugo
  * (C) 2016 Augustin Cavalier
  *
- * NOTE: This script requires abou 2GB of RAM. nodejs limits you to 1.5GB by default.
+ * NOTE: This script requires about 3GB of RAM. nodejs limits you to 1.5GB by default.
  * Override it by running this script with "node --max_old_space_size=4096 drupal2hugo.js".
  */
 
@@ -97,7 +97,12 @@ tags = [TAGS]
 // author = "AUTHOR"
 
 var base = "newsite/content";
-function GetSavePath(path, type, node) {
+function GetSavePath(path, type, node, ext) {
+	if (!ext) {
+		console.log("No extension for node ", node);
+		process.exit(1);
+	}
+
 	var countSlash = (path.match(/\//g) || []).length;
 	if ((type == "blog" && countSlash == 3) ||
 		(type == "content_news" && countSlash == 2)) {
@@ -113,38 +118,60 @@ function GetSavePath(path, type, node) {
 		if (fs.existsSync(ret)) {
 			// Path exists. Are we at the end?
 			if (i == (path.length - 1)) {
-				// Check if there already is an 'index.html' file
-				ret += "/index.html";
-				if (fs.existsSync(ret)) {
+				// Check if there already is an index file.
+				ret += "/index";
+				if (fs.existsSync(ret + ".txt") || fs.existsSync(ret + ".md") || fs.existsSync(ret + ".html")) {
 					console.error("FATAL: could not find unused path for", path.join('/'), node);
 					process.exit(1);
-				} else // Nope, doesn't exist, so let's use it.
+				} else { // Nope, doesn't exist, so let's use it.
+					ret += ext;
 					break;
+				}
 			} else // We aren't at the end, just continue.
 				continue;
 		} else {
 			// Path does not exist. Are we at the end?
 			if (i == (path.length - 1)) {
 				// This is it.
-				if (fs.existsSync(ret + ".html")) {
+				if (fs.existsSync(ret + ".txt") || fs.existsSync(ret + ".md") || fs.existsSync(ret + ".html")) {
 					console.error("FATAL: file already exists for", path.join('/'), node);
 					process.exit(1);
 				}
-				ret += ".html";
+				ret += ext;
 				break;
 			} else {
 				// We aren't at the end, and the path does not exist. Create a directory.
 				fs.mkdirSync(ret);
 				// Is there a file with the name of the folder we just made?
-				if (fs.existsSync(ret + ".html")) {
-					// Make it the index.html of the subdirectory.
+				// If so, make it the index.html of the subdirectory.
+				if (fs.existsSync(ret + ".html"))
 					fs.renameSync(ret + ".html", ret + "/index.html");
-				}
+				else if (fs.existsSync(ret + ".md"))
+					fs.renameSync(ret + ".md", ret + "/index.md");
+				else if (fs.existsSync(ret + ".txt"))
+					fs.renameSync(ret + ".txt", ret + "/index.txt");
 			}
 		}
 	}
 	return ret;
 }
+
+/* "format" types are apparently:
+ *    2: PHP code
+ *    3: Full HTML
+ *    5: Text w/limited HTML
+ *    8: Text w/very limited HTML
+ *    10: Plain text
+ */
+var FormatMap = {
+	"0": ".html",
+	"2": ".html",
+	"3": ".html",
+	"5": ".md",
+	"8": ".md",
+	"10": ".txt"
+};
+var hasPhpCode = [];
 for (var i in nodes) {
 	if (nodes[i][2] == "forum")
 		continue;
@@ -157,6 +184,7 @@ for (var i in nodes) {
 		changed = node[7],
 		content = undefined,
 		url_dst = undefined,
+		format = undefined,
 		tags = [];
 	if (!isPublished) {
 		console.log("INFO: skipping nid" + nid + ", unpublished");
@@ -168,6 +196,7 @@ for (var i in nodes) {
 		if (node_revisions[i][7] != changed)
 			continue;
 		content = node_revisions[i][4];
+		format = node_revisions[i][8];
 	}
 	if (!content) {
 		console.log("WARN: could not find content for nid" + nid);
@@ -180,10 +209,8 @@ for (var i in nodes) {
 		url_dst = url_alias[i][2];
 		break;
 	}
-	if (!url_dst) {
-		console.log("WARN: skipping nid" + nid + ", no url_dst");
-		continue;
-	}
+	if (!url_dst)
+		url_dst = url_src;
 	for (var i in term_node) {
 		if (term_node[i][0] != nid)
 			continue;
@@ -206,9 +233,37 @@ for (var i in nodes) {
 	}
 
 	content = content.replace(/\r\n/g, "\n").replace(/\r/g, "\n");
+	// Try to clean up some of the CSS classes.
+	content = content.replace(/\"box\-stop\"/g, '"alert alert-danger"');
+	content = content.replace(/\"box\-info\"/g, '"alert alert-info"');
+	content = content.replace(/\"box\-warning\"/g, '"alert alert-warning"');
+	// Clean up <code> tags.
+	for (var i = 0; i < content.length; i++) {
+		i = content.indexOf("<code>", i);
+		if (i == -1)
+			break;
+		// See if there is a linebreak before the </code>.
+		var j = content.indexOf("</code>", i), hasBreak = false;
+		for (var p = i; p < j; p++) {
+			if (content[p] == "\n") {
+				hasBreak = true;
+				break;
+			}
+		}
+		if (hasBreak) {
+			content = content.substr(0, i) + "<pre>" + content.substr(i + 6);
+			content = content.substr(0, j - 1) + "</pre>" + content.substr((j - 1) + 7);
+		}
+	}
+
 	outfile += content;
 
-	var file = GetSavePath(url_dst, type, node);
+	var file = GetSavePath(url_dst, type, node, FormatMap[format]);
 	console.log("INFO: writing (nid" + nid + ")", file);
+	if (format == "2")
+		hasPhpCode.push(file);
 	fs.writeFileSync(file, outfile);
 }
+
+console.log("");
+console.log("The following pages have PHP code in them and should be audited:", hasPhpCode);
