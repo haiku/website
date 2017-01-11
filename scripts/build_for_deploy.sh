a778c0975612e541531fc93028404dd623f500b6
Augustin Cavalier <waddlesplash@gmail.com>
2017-01-11 15:52:53 -0500

    scripts: Add a build_for_deploy.sh which downloads the userguide.

---
+++ scripts/build_for_deploy.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+set -e
+for cmd in "mv rm wget unzip hugo"; do
+	command -v $cmd >/dev/null 2>&1 || { echo -e >&2 "$cmd is not installed, please install it."; exit 1; }
+done
+
+if [ ! -d content ]; then
+	echo "This script must be run from the root of the repository."
+	exit 1
+fi
+
+wget https://github.com/haiku/haiku/archive/master.zip -O master.zip -nv
+unzip master.zip 'haiku-master/docs/userguide/**'
+rm master.zip
+
+hugo
+mv haiku-master/docs public/
+rm -rf haiku-master/
