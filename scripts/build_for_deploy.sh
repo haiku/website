6e82b09825b468f4e4433a77635aa76c874878e1
Augustin Cavalier <waddlesplash@gmail.com>
2017-01-16 22:33:31 -0500

    build_for_deploy: Try for Hugo 0.18 if installed.

--- scripts/build_for_deploy.sh
+++ scripts/build_for_deploy.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
 set -e
-for cmd in "mv rm wget unzip sed hugo"; do
+HUGO=hugo
+for cmd in "mv rm wget unzip sed $HUGO"; do
 	command -v $cmd >/dev/null 2>&1 || { echo -e >&2 "$cmd is not installed, please install it."; exit 1; }
 done
+command -v hugo_0.18 >/dev/null 2>&1 && HUGO=hugo_0.18
+echo "Using Hugo $HUGO"
 
 if [ ! -d content ]; then
 	echo "This script must be run from the root of the repository."
@@ -17,7 +20,7 @@ rm master.zip
 rm -rf public/
 
 sed -i "s/BuildTypeIsDeploy = false/BuildTypeIsDeploy = true/g" config.toml
-hugo
+$HUGO
 sed -i "s/BuildTypeIsDeploy = true/BuildTypeIsDeploy = false/g" config.toml
 
 mv haiku-master/docs public/
