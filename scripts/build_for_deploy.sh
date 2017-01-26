a2752859bb1fffd59efc0bad10a4e6c2d4b30cdf
Augustin Cavalier <waddlesplash@gmail.com>
2017-01-25 21:03:44 -0500

    scripts/build_for_deploy.sh: Switch to using a git checkout of Haiku.
    Also, first attempts at Doxygen.

--- scripts/build_for_deploy.sh
+++ scripts/build_for_deploy.sh
@@ -6,7 +6,6 @@ for cmd in "mv rm wget unzip sed $HUGO"; do
 	command -v $cmd >/dev/null 2>&1 || { echo -e >&2 "$cmd is not installed, please install it."; exit 1; }
 done
 command -v hugo_0.18 >/dev/null 2>&1 && HUGO=hugo_0.18
-which doxygen || true
 echo "Using Hugo $HUGO"
 
 if [ ! -d content ]; then
@@ -14,10 +13,25 @@ if [ ! -d content ]; then
 	exit 1
 fi
 
-wget https://github.com/haiku/haiku/archive/master.zip -O master.zip -nv
-unzip -q master.zip 'haiku-master/docs/userguide/**'
-unzip -q master.zip 'haiku-master/docs/welcome/**'
-rm master.zip
+if [ ! -d haiku ]; then
+	git clone https://github.com/haiku/haiku/ --depth=5
+fi
+cd haiku
+git pull --ff-only
+if [ ! -d generated ]; then
+	mkdir generated
+	cd generated
+	wget https://ftp.stack.nl/pub/users/dimitri/doxygen-1.8.13.linux.bin.tar.gz -O doxygen.tar.gz -nv
+	tar -xvf doxygen.tar.gz
+	mkdir doxybin
+	cp doxygen-*/bin/* doxybin/
+	rm -rf doxygen-*/
+	rm doxygen.bin.tar.gz
+	cd ..
+fi
+generated/doxybin/doxygen --help || true
+rm -rf generated
+cd ..
 
 rm -rf public/docs/
 
@@ -25,5 +39,6 @@ sed -i "s/BuildTypeIsDeploy = false/BuildTypeIsDeploy = true/g" config.toml
 $HUGO
 sed -i "s/BuildTypeIsDeploy = true/BuildTypeIsDeploy = false/g" config.toml
 
-mv haiku-master/docs public/
-rm -rf haiku-master/
+mkdir public/docs/
+cp -R haiku/docs/userguide/ public/docs/
+cp -R haiku/docs/welcome/ public/docs/
