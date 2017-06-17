af40781a988234da2fe8d2b61f0b058ca995cbdf
Augustin Cavalier <waddlesplash@gmail.com>
2017-06-17 14:24:00 -0400

    build_for_deploy: Remove Hugo version detection altogether.
    Netlify now has a mechanism to specify version in site config.

--- scripts/build_for_deploy.sh
+++ scripts/build_for_deploy.sh
@@ -5,7 +5,6 @@ HUGO=hugo
 for cmd in "mv rm wget unzip sed $HUGO"; do
 	command -v $cmd >/dev/null 2>&1 || { echo -e >&2 "$cmd is not installed, please install it."; exit 1; }
 done
-command -v hugo_0.22 >/dev/null 2>&1 && HUGO=hugo_0.22
 echo "Using Hugo $HUGO"
 
 if [ ! -d content ]; then
