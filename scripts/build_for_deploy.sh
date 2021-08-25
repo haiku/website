6f3f768adaf2c470161098db11a74f34e1d0feec
Adrien Destugues <adrien.destugues@opensource.viveris.fr>
2021-08-25 22:20:54 +0200

    Generate developer/internals documentation

--- scripts/build_for_deploy.sh
+++ scripts/build_for_deploy.sh
@@ -2,7 +2,7 @@
 
 set -e
 HUGO=hugo
-for cmd in "mv rm wget unzip sed $HUGO"; do
+for cmd in "mv rm wget unzip sed pip $HUGO"; do
 	command -v $cmd >/dev/null 2>&1 || { echo -e >&2 "$cmd is not installed, please install it."; exit 1; }
 done
 echo "Using Hugo $HUGO"
@@ -15,25 +15,31 @@ fi
 if [ ! -d haiku ]; then
 	git clone https://github.com/haiku/haiku/ --depth=5
 fi
-cd haiku
+pushd haiku
 	if [ ! -d generated ]; then
 		mkdir generated
-		cd generated
+		pushd generated
 		wget https://nchc.dl.sourceforge.net/project/doxygen/rel-1.8.16/doxygen-1.8.16.linux.bin.tar.gz -O doxygen.tar.gz -nv
 		tar -xvf doxygen.tar.gz doxygen-1.8.16/bin/
 		mkdir doxybin
 		mv doxygen-*/bin/* doxybin/
 		rm -rf doxygen-*/
 		rm doxygen.tar.gz
-		cd ..
+		popd
+		pip install sphinx
 	fi
 	gitout=$(git pull --ff-only)
 	if [[ $gitout != *"Already up-to-date"* ]] || [ ! -d "generated/doxygen/html" ]; then
-		cd docs/user/
+		pushd docs/user/
 			../../generated/doxybin/doxygen
-		cd ../..
+		popd
 	fi
-cd ..
+	if [[ $gitout != *"Already up-to-date"* ]] || [ ! -d "docs/develop/generated/html" ]; then
+		pushd docs/develop/
+			make html
+		popd
+	fi
+popd
 
 rm -rf public/docs/
 
@@ -46,6 +52,7 @@ cp -R haiku/docs/userguide/ public/docs/
 cp -R haiku/docs/welcome/ public/docs/
 cp -R haiku/docs/interface_guidelines/ public/docs/
 cp -R haiku/generated/doxygen/html/ public/docs/
+cp -R haiku/docs/develop/generated/html public/docs/develop
 
 mv public/docs/html/ public/docs/api/
 mv public/docs/interface_guidelines/ public/docs/HIG/
